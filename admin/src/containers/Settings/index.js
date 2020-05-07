d180fae38919e33b30c903555a9c4d8a86d30e9d
Hugues BUREAU <h.bureau@uwizy.com>
2020-05-07 16:12:42 +0200

    feat: add about page content

--- admin/src/containers/Settings/index.js
+++ admin/src/containers/Settings/index.js
@@ -1,16 +1,30 @@
-import React from 'react';
+import React from 'react'; // eslint-disable-line import/no-unresolved
 import PropTypes from 'prop-types';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom'; // eslint-disable-line import/no-unresolved
+import { Button } from '@buffetjs/core'; // eslint-disable-line import/no-unresolved
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'; // eslint-disable-line import/no-unresolved
+import { faGithub } from '@fortawesome/free-brands-svg-icons'; // eslint-disable-line import/no-unresolved
 import pluginId from '../../pluginId';
 
 const GeneralBlock = () => (
   <div>
-    <h1>Setting Page 1</h1>
+    <h1>General</h1>
   </div>
 );
 const AboutBlock = () => (
   <div>
-    <h1>Setting Page 2</h1>
+    <h1>About</h1>
+    <p>
+      This plugin has been developed by{' '}
+      <a href="https://uwizy.com" target="_blank" rel="noopener noreferrer">
+        Uwizy
+      </a>
+      <hr />
+      <Button color="secondary" icon={<FontAwesomeIcon icon={faGithub} />}>
+        Github
+      </Button>
+      <br />
+    </p>
   </div>
 );
 
